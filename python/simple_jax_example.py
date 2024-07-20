23c19c60e55af46f9f996aa6e563ac115a459839
Joao Sousa-Pinto <joaospinto@gmail.com>
2024-07-20 13:12:56 -0700

    Add MLIR bytecode exporting to the Python code.

--- python/simple_jax_example.py
+++ python/simple_jax_example.py
@@ -1,7 +1,12 @@
 # Useful references:
 # 1. https://jax.readthedocs.io/en/latest/aot.html.
 # 2. https://jax.readthedocs.io/en/latest/export/export.html
-# 3. https://openxla.org/stablehlo/tutorials/jax-export
+# 3. https://jax.readthedocs.io/en/latest/jax.stages.html#jax.stages.Lowered.compiler_ir
+# 4. https://jax.readthedocs.io/en/latest/_autosummary/jax.export.Exported.html#jax.export.Exported.mlir_module_serialized
+# 5. https://jax.readthedocs.io/en/latest/export/export.html#export-calling-convention-version
+# 6. https://openxla.org/stablehlo/tutorials/jax-export
+# 7. https://openxla.org/stablehlo/vhlo
+# 8. https://openxla.org/stablehlo/compatibility
 
 import jax
 
@@ -18,20 +23,27 @@ def main():
     dummy = 3.0  # The value is discarded.
 
     lowered = jit_f.lower(dummy)
-    hlo = lowered.compile().as_text()
+    exported = export.export(jit_f)(dummy)
+
+    # These are intended as debug-only serializations. Not portable!
+    hlo = lowered.compiler_ir("hlo").as_hlo_text()
+    serialized_hlo_proto = lowered.compiler_ir("hlo").as_serialized_hlo_module_proto()
+    stable_hlo = str(lowered.compiler_ir("stablehlo"))
+
+    # This is portable.
+    mlir_bc = exported.mlir_module_serialized
 
     with open("hlo/example1.txt", "w") as hlo_f:
         hlo_f.write(hlo)
 
-    stable_hlo = export.export(jit_f)(dummy).mlir_module()
+    with open("hlo/example1.binpb", "wb") as hlo_bf:
+        hlo_bf.write(serialized_hlo_proto)
 
-    with open("stable_hlo/example1.txt", "w") as hlo_f:
-        hlo_f.write(stable_hlo)
+    with open("stable_hlo/example1.txt", "w") as stable_hlo_f:
+        stable_hlo_f.write(stable_hlo)
 
-    serialized_proto = lowered.compiler_ir("hlo").as_serialized_hlo_module_proto()
-
-    with open("hlo/example1.binpb", "wb") as bf:
-        bf.write(serialized_proto)
+    with open("mlir/example1.bc", "wb") as bc_f:
+        bc_f.write(mlir_bc)
 
 
 if __name__ == "__main__":
